bf1cbee6271b0f74178ba77ef135384c6c1fa9d2
Benjamin Jordan <bmj465@gmail.com>
2017-08-19 19:39:58 -0700

    Added Void type, released v 0.2.2.

---
+++ CreateAR.Commons.Unity.Async/Void.cs
@@ -0,0 +1,21 @@
+﻿namespace CreateAR.Commons.Unity.Async
+{
+    /// <summary>
+    /// Void type for use within the Async package.
+    /// </summary>
+    public sealed class Void
+    {
+        /// <summary>
+        /// Static instance.
+        /// </summary>
+        public static Void Instance = new Void();
+
+        /// <summary>
+        /// Keep it private.
+        /// </summary>
+        private  Void()
+        {
+            
+        }
+    }
+}
