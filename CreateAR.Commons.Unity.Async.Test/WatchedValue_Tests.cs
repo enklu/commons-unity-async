eff90dde765bec7e87a578bd50d3aeb75da1a3ac
Benjamin Jordan <bmj465@gmail.com>
2017-08-25 10:33:49 -0700

    Added WatchedValue implementation. Bumped to v.0.4.0.

---
+++ CreateAR.Commons.Unity.Async.Test/WatchedValue_Tests.cs
@@ -0,0 +1,175 @@
+﻿using NUnit.Framework;
+
+namespace CreateAR.Commons.Unity.Async
+{
+    [TestFixture]
+    public class WatchedValue_Tests
+    {
+        public class TestClass
+        {
+            public string Foo;
+        }
+
+        [Test]
+        public void CallWatcher()
+        {
+            var called = false;
+            var instance = new TestClass();
+
+            var watcher = new WatchedValue<TestClass>();
+            watcher.OnChanged += value =>
+            {
+                called = true;
+
+                Assert.AreSame(instance, value);
+            };
+
+            watcher.Value = instance;
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void MissWatcherConstructor()
+        {
+            var called = false;
+            var instance = new TestClass();
+
+            var watcher = new WatchedValue<TestClass>(instance);
+            watcher.OnChanged += value =>
+            {
+                called = true;
+
+                Assert.AreSame(instance, value);
+            };
+
+            Assert.IsFalse(called);
+        }
+
+        [Test]
+        public void CallWatcherMultiple()
+        {
+            var numCalled = 0;
+            
+            var watcher = new WatchedValue<TestClass>();
+            watcher.OnChanged += value =>
+            {
+                numCalled++;
+            };
+
+            watcher.Value = new TestClass();
+            watcher.Value = new TestClass();
+            watcher.Value = new TestClass();
+
+            Assert.AreEqual(3, numCalled);
+        }
+
+        [Test]
+        public void IgnoreEqualValues()
+        {
+            var numCalled = 0;
+
+            var watcher = new WatchedValue<TestClass>();
+            watcher.OnChanged += value =>
+            {
+                numCalled++;
+            };
+
+            var instance = new TestClass();
+            watcher.Value = instance;
+            watcher.Value = instance;
+            watcher.Value = instance;
+
+            Assert.AreEqual(1, numCalled);
+        }
+
+        [Test]
+        public void CallReady()
+        {
+            var called = false;
+            var instance = new TestClass();
+
+            var watcher = new WatchedValue<TestClass>();
+            watcher
+                .Ready()
+                .OnSuccess(value =>
+                {
+                    called = true;
+
+                    Assert.AreSame(instance, value);
+                });
+
+            watcher.Value = instance;
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void CallReadyRetro()
+        {
+            var called = false;
+            var instance = new TestClass();
+
+            var watcher = new WatchedValue<TestClass>();
+            watcher.Value = instance;
+            watcher
+                .Ready()
+                .OnSuccess(value =>
+                {
+                    called = true;
+
+                    Assert.AreSame(instance, value);
+                });
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void CallReadyConstructor()
+        {
+            var called = false;
+            var instance = new TestClass();
+
+            var watcher = new WatchedValue<TestClass>();
+            watcher.Value = instance;
+            watcher
+                .Ready()
+                .OnSuccess(value =>
+                {
+                    called = true;
+
+                    Assert.AreSame(instance, value);
+                });
+
+            Assert.IsTrue(called);
+        }
+
+        [Test]
+        public void CallReadyIndividualTokens()
+        {
+            var called = false;
+            var abortCalled = false;
+            var instance = new TestClass();
+
+            var watcher = new WatchedValue<TestClass>();
+            watcher
+                .Ready()
+                .OnSuccess(value =>
+                {
+                    called = true;
+                });
+            watcher
+                .Ready()
+                .OnSuccess(value =>
+                {
+                    abortCalled = true;
+                })
+                .Abort();
+
+            watcher.Value = instance;
+
+            Assert.IsTrue(called);
+            Assert.IsFalse(abortCalled);
+        }
+    }
+}
---
+++ CreateAR.Commons.Unity.Async/WatchedValue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace CreateAR.Commons.Unity.Async
+{
+    /// <summary>
+    /// Represents a value that can be watched.
+    /// </summary>
+    /// <typeparam name="T">Type of the value.</typeparam>
+    public sealed class WatchedValue<T> where T : class
+    {
+        /// <summary>
+        /// Internal token used to track ready state.
+        /// </summary>
+        private readonly AsyncToken<T> _readyToken = new AsyncToken<T>();
+
+        /// <summary>
+        /// Backing value for Value property.
+        /// </summary>
+        private T _value;
+        
+        /// <summary>
+        /// Value Accessor
+        /// </summary>
+        public T Value
+        {
+            get => _value;
+            set
+            {
+                if (_value == value)
+                {
+                    return;
+                }
+
+                _value = value;
+
+                // resolve ready-- will only fire the first time
+                _readyToken.Succeed(_value);
+
+                // call event
+                OnChanged?.Invoke(_value);
+            }
+        }
+
+        /// <summary>
+        /// Called when the value has been changed.
+        /// </summary>
+        public event Action<T> OnChanged;
+
+        /// <summary>
+        /// Creates a WatchedValue.
+        /// </summary>
+        public WatchedValue()
+        {
+            // empty
+        }
+
+        /// <summary>
+        /// Creates a WatchedValue with an initial value.
+        /// </summary>
+        /// <param name="value">The initial value.</param>
+        public WatchedValue(T value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// Returns a token that is resolved when the value is first set.
+        /// </summary>
+        /// <returns></returns>
+        public IAsyncToken<T> Ready()
+        {
+            return _readyToken.Token();
+        }
+    }
+}
