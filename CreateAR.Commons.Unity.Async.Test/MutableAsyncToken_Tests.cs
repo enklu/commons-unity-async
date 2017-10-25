c4f64797ba955477de1e1a9c0bdbb3dece5ca8a7
Benjamin Jordan <bmj465@gmail.com>
2017-10-25 10:24:12 -0700

    Initial commit.

---
+++ CreateAR.Commons.Unity.Async.Test/MutableAsyncToken_Tests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CreateAR.Commons.Unity.Async
+{
+    [TestFixture]
+    public class MutableAsyncToken_Tests
+    {
+        public class MyClass
+        {
+            public string Foo = Guid.NewGuid().ToString();
+        }
+
+        [Test]
+        public void Succeed()
+        {
+            var isCalled = false;
+            var value = new MyClass();
+            
+            var token = new MutableAsyncToken<MyClass>();
+            token
+                .OnSuccess(result =>
+                {
+                    isCalled = true;
+
+                    Assert.AreSame(value, result);
+                })
+                .OnFailure(exception => throw exception);
+
+            Assert.IsTrue(isCalled);
+        }
+    }
+}
---
+++ CreateAR.Commons.Unity.Async/MutableAsyncToken.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CreateAR.Commons.Unity.Async
+{
+    public interface IMutableAsyncToken<T> : IAsyncToken<T>
+    {
+        //
+    }
+
+    public class MutableAsyncToken<T> : IMutableAsyncToken<T>
+    {
+        public IAsyncToken<T> OnSuccess(Action<T> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncToken<T> OnFailure(Action<Exception> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncToken<T> OnFinally(Action<IAsyncToken<T>> callback)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncToken<T> Abort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncToken<T> Token()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
